dd848a6da2bb7b37351f6ec4a827ab8fbbeda84d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-05-21 11:45:03 +0900

    update: how we can use props

--- react-mini-demo/src/App.js
+++ react-mini-demo/src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>TODOs</h1>
-      <Todo />
+      <Todo text="Task1" />
+      <Todo text="Task2" />
+      <Todo text="Task3" />
     </div>
   );
 }
