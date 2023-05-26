01c765536f5dd1e78d1b362bf2afaee1d115eec9
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-05-26 15:25:36 +0900

    update: nested routing

---
+++ nextjs-about-routing/pages/portfolio/index.js
@@ -0,0 +1,10 @@
+function PortfolioPage() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+    </div>
+  );
+}
+
+export default PortfolioPage;
+// Path: n/pages/portfolio/index.js
---
+++ nextjs-about-routing/pages/portfolio/list.js
@@ -0,0 +1,9 @@
+function ListPage() {
+  return (
+    <div>
+      <h1>ListPage</h1>
+    </div>
+  );
+}
+
+export default ListPage;
