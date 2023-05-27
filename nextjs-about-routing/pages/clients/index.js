c5441b1cc030613d170609b81e83f038b9d64aea
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-05-27 14:42:40 +0900

    update: example of link in nextjs

--- nextjs-about-routing/pages/clients/index.js
+++ nextjs-about-routing/pages/clients/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "test", name: "Test Client" },
+    { id: "test2", name: "Test2 Client" },
+  ];
+
   return (
     <div>
       <h1>Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
