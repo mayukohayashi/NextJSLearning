9d1fa19dc32708a972a1b5183611de302e839c70
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-05-27 15:02:33 +0900

    update: navigating in nextjs

--- nextjs-about-routing/pages/clients/[id]/index.js
+++ nextjs-about-routing/pages/clients/[id]/index.js
@@ -1,7 +1,20 @@
+import { useRouter } from "next/router";
+
 function ClientProjectPage() {
+  const router = useRouter();
+  console.log(router.query);
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientid]",
+      query: { id: "test", clientid: "testproject" },
+    });
+  }
+
   return (
     <div>
       <h1>The project of a givin client</h1>
+      <button onClick={loadProjectHandler}>Loading test</button>
     </div>
   );
 }
