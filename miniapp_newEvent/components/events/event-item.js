4170b7ce8d00183869eacccb2e25bd31ee7ed90d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-06-13 13:20:04 +0900

    add: icons on list page

--- miniapp_newEvent/components/events/event-item.js
+++ miniapp_newEvent/components/events/event-item.js
@@ -1,7 +1,9 @@
-import Link from "next/link";
 import Button from "../ui/button";
-import classes from './event-item.module.css';
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
+import classes from './event-item.module.css';
 
 function EventItem(props) {
   const {title, image, date, location, id} = props;
@@ -21,14 +23,21 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+          <DateIcon />
             <time>{readableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+              <span className={classes.icon}>
+                <ArrowRightIcon />
+              </span>
+          </Button>
         </div>
       </div>
     </li>
