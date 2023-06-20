1fd4f74e7033d259db217bef680dc7d5b7a8e01f
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-06-20 17:57:51 +0900

    add: event-detail folder

--- miniapp_newEvent/pages/events/[eventId].js
+++ miniapp_newEvent/pages/events/[eventId].js
@@ -1,6 +1,18 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+
 function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId)
+
+  if (!event) {
+    return <p>No event found</p>
+  }
+
   return (
-    <div>Event Detail</div>
+    <div><h1>Event Detail</h1></div>
   );
 }
 
