ae5ea29061fb72803329f7872aac824f88c602c1
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-06-26 13:36:37 +0900

    update: filter

--- miniapp_newEvent/components/events/events-search.js
+++ miniapp_newEvent/components/events/events-search.js
@@ -1,14 +1,28 @@
+import { useRef } from "react";
+
 import Button from "../ui/button";
 import classes from "./events-search.module.css";
 
 function EventsSearch(props)
 {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function submitHandler(event){
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  }
+
   return(
     <form className={classes.form}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
             <option value="2023">2023</option>
@@ -16,7 +30,7 @@ function EventsSearch(props)
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthInputRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
--- miniapp_newEvent/pages/events/[...slug].js
+++ miniapp_newEvent/pages/events/[...slug].js
@@ -1,4 +1,32 @@
+import {useRouter} from "next/router";
+
 function FilteredEventPage() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if(!filterData) {
+    return <p className="center">Loading...</p>
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2020 || numMonth < 1 || numMonth > 12) {
+    return <p> Invalid Filtered</p>
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year : numYear,
+    mont : numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found</p>
+  }
+
   return (
     <div>Filtered Event!</div>
   );
