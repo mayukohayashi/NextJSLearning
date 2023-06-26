17f3bb743c7420ea00d1bf9d733d99552591510a
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-06-26 12:27:50 +0900

    add: event-search page

--- miniapp_newEvent/components/ui/button.js
+++ miniapp_newEvent/components/ui/button.js
@@ -2,11 +2,16 @@ import Link from "next/link";
 import classes from './button.module.css';
 
 function Button(props) {
-  return(
-    <Link href={props.link} className={classes.btn}>
-      {props.children}
-    </Link>
-  );
+
+  if (props.link){
+    return(
+      <Link href={props.link} className={classes.btn}>
+        {props.children}
+      </Link>
+    );
+  }
+
+  return <button className={classes.btn} onClick={props.onClick}>{props.children}</button>
 }
 
 export default Button;
